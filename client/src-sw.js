28db0afdf40daca79d440616db750499f55c1345
Ash <ashloraptor@gmail.com>
2024-04-02 11:54:28 -0400

    Worked on src-sw.js file

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,9 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+//ref from mini project
+const { StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,4 +30,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(  
+
+    //ref from mini project
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  // Name of the cache storage.
+  cacheName: 'asset-cache',
+  plugins: [
+    // This plugin will cache responses with these headers to a maximum-age of 30 days
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+}) //end ref
+);
